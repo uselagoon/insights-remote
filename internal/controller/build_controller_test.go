ccaf4af1031634367808ef1df10f8052ed899300
Brandon Williams <brandon.williams@amazee.io>
2026-02-09 08:06:41 -0600

    feat: copy feature flag vars from build pod to scan pod

--- internal/controller/build_controller_test.go
+++ internal/controller/build_controller_test.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
-	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"reflect"
 	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func Test_generateScanPodSpec(t *testing.T) {
@@ -12,6 +14,7 @@ func Test_generateScanPodSpec(t *testing.T) {
 		images    []string
 		buildName string
 		namespace string
+		buildPod  *corev1.Pod
 	}
 	tests := []struct {
 		name    string
@@ -20,16 +23,39 @@ func Test_generateScanPodSpec(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "No images",
-			args:    args{images: nil, namespace: "testns", buildName: "buildnamehere"},
+			name: "No images to scan",
+			args: args{
+				images:   nil,
+				buildPod: &corev1.Pod{},
+			},
 			wantErr: true,
 		},
 		{
-			name: "FoundImages",
+			name: "Basic images to scan",
 			args: args{
-				images:    []string{"image1", "image2"},
-				namespace: "testns",
-				buildName: "buildnamehere",
+				images: []string{"image1", "image2"},
+				buildPod: &corev1.Pod{
+					ObjectMeta: v12.ObjectMeta{
+						Namespace: "testns",
+						Name:      "buildnamehere",
+					},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Env: []corev1.EnvVar{
+									{
+										Name:  "PROJECT",
+										Value: "projectName",
+									},
+									{
+										Name:  "ENVIRONMENT",
+										Value: "environmentName",
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 			want: &corev1.Pod{
 				ObjectMeta: v1.ObjectMeta{
@@ -53,6 +79,118 @@ func Test_generateScanPodSpec(t *testing.T) {
 									Value: "testns",
 								},
 								{
+									Name:  "DOCKER_HOST",
+									Value: defaultDockerhost,
+								},
+								{
+									Name:  "PROJECT",
+									Value: "projectName",
+								},
+								{
+									Name:  "ENVIRONMENT",
+									Value: "environmentName",
+								},
+							},
+							ImagePullPolicy: "Always",
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "lagoon-internal-registry-secret",
+									MountPath: "/home/.docker/",
+									ReadOnly:  true,
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "lagoon-internal-registry-secret",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "lagoon-internal-registry-secret",
+									Items: []corev1.KeyToPath{
+										{Key: ".dockerconfigjson", Path: "config.json"},
+									},
+								},
+							},
+						},
+					},
+					RestartPolicy: "Never",
+				},
+			},
+		},
+		{
+			name: "Copy build pod env vars",
+			args: args{
+				images: []string{"image1", "image2"},
+				buildPod: &corev1.Pod{
+					ObjectMeta: v12.ObjectMeta{
+						Namespace: "testns",
+						Name:      "buildnamehere",
+					},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Env: []corev1.EnvVar{
+									{
+										Name:  "PROJECT",
+										Value: "projectName",
+									},
+									{
+										Name:  "ENVIRONMENT",
+										Value: "environmentName",
+									},
+									{
+										Name:  "ENVIRONMENT_ID",
+										Value: "10",
+									},
+									{
+										Name:  "LAGOON_ENVIRONMENT_VARIABLES",
+										Value: "lagoonEnvironmentVariables",
+									},
+									{
+										Name:  "LAGOON_FEATURE_FLAG_TEST",
+										Value: "lagoonFeatureFlagTest",
+									},
+									{
+										Name:  "LAGOON_FEATURE_FLAG_INSIGHTS",
+										Value: "lagoonFeatureFlagInsights",
+									},
+									{
+										Name:  "LAGOON_FEATURE_FLAG_",
+										Value: "lagoonFeatureFlag",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: &corev1.Pod{
+				ObjectMeta: v1.ObjectMeta{
+					Namespace: "testns",
+					Name:      scannerNameFromBuildname("buildnamehere"),
+					Labels:    imageScanPodLabels(),
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: "lagoon-deployer",
+					Containers: []corev1.Container{
+						{
+							Name:  "scanner",
+							Image: "scanImageName",
+							Env: []corev1.EnvVar{
+								{
+									Name:  "INSIGHT_SCAN_IMAGES",
+									Value: "image1,image2",
+								},
+								{
+									Name:  "NAMESPACE",
+									Value: "testns",
+								},
+								{
+									Name:  "DOCKER_HOST",
+									Value: defaultDockerhost,
+								},
+								{
 									Name:  "PROJECT",
 									Value: "projectName",
 								},
@@ -61,8 +199,16 @@ func Test_generateScanPodSpec(t *testing.T) {
 									Value: "environmentName",
 								},
 								{
-									Name:  "DOCKER_HOST",
-									Value: dockerhost,
+									Name:  "LAGOON_ENVIRONMENT_VARIABLES",
+									Value: "lagoonEnvironmentVariables",
+								},
+								{
+									Name:  "LAGOON_FEATURE_FLAG_TEST",
+									Value: "lagoonFeatureFlagTest",
+								},
+								{
+									Name:  "LAGOON_FEATURE_FLAG_INSIGHTS",
+									Value: "lagoonFeatureFlagInsights",
 								},
 							},
 							ImagePullPolicy: "Always",
@@ -95,7 +241,7 @@ func Test_generateScanPodSpec(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := generateScanPodSpec(tt.args.images, "scanImageName", tt.args.buildName, tt.args.namespace, "projectName", "environmentName", dockerhost)
+			got, err := generateScanPodSpec(tt.args.buildPod, tt.args.images, "scanImageName", defaultDockerhost)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("generateScanPodSpec() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -113,6 +259,7 @@ func Test_extractDockerHost(t *testing.T) {
 		buildPod          *corev1.Pod
 		defaultDockerHost string
 		want              string
+		wantErr           bool
 	}{
 		{
 			name: "Pod with dockerhost annotation",
@@ -125,8 +272,8 @@ func Test_extractDockerHost(t *testing.T) {
 					},
 				},
 			},
-			defaultDockerHost: "docker-host.lagoon.svc",
-			want:              "custom-docker-host.svc",
+			want:    "custom-docker-host.svc",
+			wantErr: false,
 		},
 		{
 			name: "Pod without dockerhost annotation",
@@ -139,8 +286,8 @@ func Test_extractDockerHost(t *testing.T) {
 					},
 				},
 			},
-			defaultDockerHost: "docker-host.lagoon.svc",
-			want:              "docker-host.lagoon.svc",
+			want:    "",
+			wantErr: true,
 		},
 		{
 			name: "Pod with nil annotations",
@@ -151,8 +298,8 @@ func Test_extractDockerHost(t *testing.T) {
 					Annotations: nil,
 				},
 			},
-			defaultDockerHost: "docker-host.lagoon.svc",
-			want:              "docker-host.lagoon.svc",
+			want:    "",
+			wantErr: true,
 		},
 		{
 			name: "Pod with empty annotations",
@@ -163,8 +310,8 @@ func Test_extractDockerHost(t *testing.T) {
 					Annotations: map[string]string{},
 				},
 			},
-			defaultDockerHost: "docker-host.lagoon.svc",
-			want:              "docker-host.lagoon.svc",
+			want:    "",
+			wantErr: true,
 		},
 		{
 			name: "Pod with empty dockerhost annotation value",
@@ -177,26 +324,18 @@ func Test_extractDockerHost(t *testing.T) {
 					},
 				},
 			},
-			defaultDockerHost: "docker-host.lagoon.svc",
-			want:              "",
-		},
-		{
-			name: "Custom default dockerhost",
-			buildPod: &corev1.Pod{
-				ObjectMeta: v1.ObjectMeta{
-					Name:        "test-build-pod",
-					Namespace:   "test-ns",
-					Annotations: map[string]string{},
-				},
-			},
-			defaultDockerHost: "my-custom-default.svc.cluster.local",
-			want:              "my-custom-default.svc.cluster.local",
+			want:    "",
+			wantErr: true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := extractDockerHost(tt.buildPod, tt.defaultDockerHost)
+			got, err := extractDockerHost(tt.buildPod)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("extractDockerHost() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
 			if got != tt.want {
 				t.Errorf("extractDockerHost() = %v, want %v", got, tt.want)
 			}
